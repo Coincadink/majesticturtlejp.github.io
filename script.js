5d56e7a5c1c42fec082921f79f32ab3e68535223
John Peterson <johnp5419@gmail.com>
2024-08-02 00:12:19 -0600

    COMPLETE REWORK

--- script.js
+++ script.js
@@ -1,234 +1,288 @@
-const corps = [
-    "Bluecoats",
-    "Boston Crusaders",
-    "Blue Devils",
-    "Phantom Regiment",
-    "Carolina Crown",
-    "Santa Clara Vanguard",
-    "Mandarins",
-    "Blue Stars",
-    "Colts",
-    "The Cavaliers",
-    "Troopers",
-    "Blue Knights",
-    "Madison Scouts",
-    "Spirit of Atlanta",
-    "Crossmen",
-    "Pacific Crest",
-    "Music City",
-    "The Academy",
-    "Genesis",
-    "Jersey Surf",
-    "Seattle Cascades"
-];
+// Window resize callback function.
 
-const colors = [
-    '#e6194B', 
-    '#3cb44b', 
-    '#ffe119', 
-    '#4363d8', 
-    '#f58231', 
-    '#911eb4', 
-    '#42d4f4', 
-    '#f032e6', 
-    '#bfef45', 
-    '#fabed4', 
-    '#469990', 
-    '#dcbeff', 
-    '#9A6324', 
-    '#808000', 
-    '#ffd8b1', 
-    '#000075', 
-    '#a9a9a9', 
-    '#000000',
-    '#e6194B', 
-    '#3cb44b', 
-    '#ffe119', 
-]
+window.onresize = () => { location.reload(); };
 
-window.onresize = function(){ location.reload(); }
+// Get elements and flexbile dimensions.
 
-const _plot = d3.select("#plot");
-const _legend = d3.select("#legend");
+const legend = d3.select("#legend");
+const legWidth = parseFloat(legend.style("width"));
+const legHeight = parseFloat(legend.style("height"));
+
+const plot = d3.select("#plot");
+const plotWidth = parseFloat(plot.style("width"));
+const plotHeight = parseFloat(plot.style("height"));
+
+// Plot margins.
+
+const margin = {
+    "T": 20,
+    "B": 30,
+    "R": 20,
+    "L": 30,
+};
+
+//----------------------------------------------------------------------------//
+//                                    AXES                                    //
+//----------------------------------------------------------------------------//
+
+// x Axis.
 
 var xScale = null;
-var maxDate = null;
-var yScale = null;
-var tooltip = null;
+var xAxis = null;
+var xAx = null;
 
-const margin = 10.0;
+updateXAxis([0, 0], 0);
 
-const pointSize = 4.0;
+function updateXAxis(domain, ticks) {
 
-var width = 0.0;
-var height = 0.0;
+    if (xAx) xAx.remove();
 
-
-var dates = [];
-var scores = [];
-
-
-function setXAxis(dates) {
     xScale = d3.scaleTime()
-        .domain(d3.extent(dates))
-        .range([margin * 2.0, width - margin])
+        .domain(domain)
+        .range([0 + margin.L, plotWidth - margin.R]);
 
-    const xAxis = d3.axisBottom(xScale)
-        .ticks(3)
-        .tickFormat(d3.timeFormat('%b, %d'))
+    xAxis = d3.axisBottom(xScale)
+        .ticks(ticks)
+        .tickFormat(d3.timeFormat('%b, %d'));
 
-    _plot.append("g")
-        .attr("transform", `translate(0,${height - margin * 2.0})`)
-        .call(xAxis)
-}
+    xAx = plot.append("g")
+        .attr("transform", `translate(0, ${plotHeight - margin.B})`)
+        .call(xAxis);
 
-function setYAxis(scores) {
+};
+
+// y Axis.
+
+var yScale = null;
+var yAxis = null;
+var yAx = null;
+
+updateYAxis([0, 0], 0);
+
+function updateYAxis(domain, ticks = 10) {
+    
+    if (yAx) yAx.remove();
+
     yScale = d3.scaleLinear()
-        .domain(d3.extent(scores))
-        .range([height - margin * 2.0, margin]);
-    
-    const yAxis = d3.axisLeft(yScale)
-        .ticks(4)
-    
-    _plot.append("g")
-        .attr("transform", `translate(${margin * 2.0}, 0)`)
+        .domain(domain)
+        .range([plotHeight - margin.B, margin.T]);
+
+    yAxis = d3.axisLeft(yScale)
+        .ticks(ticks)
+        .tickFormat(d3.format(".1f"));
+
+    yAx = plot.append("g")
+        .attr("transform", `translate(${margin.L}, 0)`)
         .call(yAxis);
-}
 
-function createTooltip() {
-    tooltip = _plot.append("text")
-        .attr("class", "tooltip")
-        .attr("x", margin*3)
-        .attr("y", margin)
-        .attr("dy", 0)
-        .attr("fill", "black")
+};
+
+//----------------------------------------------------------------------------//
+//                                   LEGEND                                   //
+//----------------------------------------------------------------------------//
+
+// List of corps.
+
+const WORLD_CLASS = [
+    "Blue Devils",
+    "Blue Knights",
+    "Blue Stars",
+    "Bluecoats",
+    "Boston Crusaders",
+    "Carolina Crown",
+    "Colts",
+    "Crossmen",
+    "Genesis",
+    "Jersey Surf",
+    "Madison Scouts",
+    "Mandarins",
+    "Music City",
+    "Pacific Crest",
+    "Phantom Regiment",
+    "Santa Clara Vanguard",
+    "Seattle Cascades",
+    "Spirit of Atlanta",
+    "The Academy",
+    "The Cavaliers",
+    "Troopers",
+];
+
+const OPEN_CLASS = [
+    // "7th Regiment",
+    "Blue Devils B",
+    "Blue Devils C",
+    "Colt Cadets",
+    "Columbians",
+    "Gold",
+    "Golden Empire",
+    "Guardians",
+    "Impulse",
+    "Les Stentors",
+    "Raiders",
+    "River City Rhythm",
+    "Spartans",
+    "The Battalion",
+    "Vessel",
+];
+
+function createLegend(division, corpsList, xpos) {
+
+    // Background for corps list.
+    
+    var backing = plot.append("rect")
+        .attr("width", 200)
+        .attr("height", 350)
+        .attr("x", xpos - 100)
+        .attr("y", plotHeight - 440)
+        .attr("fill-opacity", 0)
+        .attr("rx", 50)
+
+    // Add corps lists.
+
+    const legendText = plot.append("text")
+
+    legendText // TODO: ADD A SELECT ALL OPTION
         .selectAll("tspan")
-        .data(["", "", ""])
-            .join("tspan")
-            .text(d => `${d}`)
+        .data(corpsList)
+        .join("tspan")
+            .attr("class", "corps")
+            .text(d => d)
+            .attr("x", xpos)
             .attr("dy", "1.0em")
-            .attr("x", margin*3)
-}
-
-function tooltipCallback(evt, d, corp) {
-    tooltip
-        .data([corp, d.show, parseFloat(d.score).toFixed(3)])
-            .join(
-                enter => enter.append("tspan").text(d => `${d}`),
-                update => update.text(d => `${d}`),
-                exit => exit.remove()
-            )
-}
-
-function plotPoints([corp, scores], color) {
-
-    var _data = _plot.append("g")
-        .attr("id", corp)
-
-    _data.append("path")
-        .attr("fill", "none")
-        .attr("stroke", color)
-        .attr("stroke-width", 1.5)
-        .datum(scores)
-        .attr("d", d3.line()
-            .x(d => xScale(d.date))
-            .y(d => yScale(d.score))
-        )
-
-    _data.append("g")
-        .selectAll("circle")
-        .data(scores)
-        .join("circle")
-            .attr("cx", d => xScale(d.date))
-            .attr("cy", d => yScale(d.score))
-            .attr("r", pointSize)
-            .attr("fill", color)
-
-        .on("mouseover mousedrag", (evt, d) => { tooltipCallback(evt, d, corp); })
-        .on("mouseout", () => { tooltip.text(""); });
-}
-
-
-function plot(data) {
-
-    width = parseFloat(_plot.style("width"));
-    height = parseFloat(_plot.style("height"));
-
-    console.log(`Width: ${width}, Height: ${height}`);
+            .on("click",
+                (evt, d) => {
+                    currentOpacity = d3.selectAll("." + d.replaceAll(" ", "-")).style("opacity");
+                    if (currentOpacity == 1) {
+                        d3.selectAll("." + d.replaceAll(" ", "-")).transition().style("opacity", 0);
+                        d3.select(evt.target).attr("fill", "black");
+                    } else {
+                        d3.selectAll("." + d.replaceAll(" ", "-")).transition().style("opacity", 1);
+                        d3.select(evt.target).attr("fill", "blue");
+                    }
+                }
+            );
     
-    dates = data.map(([_, shows]) => shows.map(show => show.date));
-    scores = data.map(([_, shows]) => shows.map(show => show.score));
+    var bbox = legendText.node().getBBox()
     
-    maxDate = d3.max(dates.flat())
+    legendText
+        .attr("y", plotHeight - bbox.height - 100) // Place on bottom of plot svg.
+        .attr("display", "none");
 
-    setXAxis(dates.flat());
-    setYAxis(scores.flat());
+    // Adds division text.
 
-    createTooltip();
+    legend.append("text")
+        .text(division)
+        .attr("class", "division")
+        .attr("x", xpos)
+        .attr("y", legHeight / 2)
+        .attr("font-size", "6vh")
+        .on("click", (evt, d) => { 
+            legendText.attr("display", legendText.attr("display") === "none" ? "inline" : "none")
+            backing.attr("fill-opacity", backing.attr("fill-opacity") == 0.05 ? 0 : 0.05)
+        });
 
-    for (const [i, pair] of data.entries()) {
-        plotPoints(pair, colors[i]);
-    }
-}
+};
 
-function legend(data) {
+//----------------------------------------------------------------------------//
+//                                    PLOT                                    //
+//----------------------------------------------------------------------------//
 
-    const final = data.map(subdata => subdata[1][subdata[1].length - 1].score);
-    const corps = data.map(subdata => subdata[0]);
+// Return array of caption scores for a given year and corp.
+function getCorp(data, _corp, _year, _caption) { // TODO: REWRITE AND VIX VARS
+    
+    results = []
+    
+    data.forEach(
+        (show) => {
+            if (show.Date.getFullYear() == _year) {
+                scores = JSON.parse(show.Scores);
+                Object.keys(scores).forEach(
+                    (div) => {                            
+                        const corps = Object.values(scores[div].Corps)
+                        const totals = scores[div][_caption]
 
-    for (var i = 0; i < final.length - 1; i++) {
-        if(final[i] - final[i+1] < 1) {
-            const avg = (final[i] + final[i+1]) / 2
-            final[i] = avg + 0.5
-            final[i+1] = avg - 0.5
+                        for (const [i, corp] of corps.entries()) {
+                            if (corp == _corp) {   
+                                const data = { 
+                                    "show": show.Title, 
+                                    "date": show.Date, 
+                                    "score": totals[i] 
+                                }
+                                results.push(data);
+                            }
+                        }
+                    }
+                )          
+            }
         }
-    }
+    )
+    results.sort((a, b) => new Date(a.date) - new Date(b.date));
+    return results;
+};
 
-    for (var i = 0; i < final.length - 1; i++) {
-        if(final[i] - final[i+1] < 1) {
-            const avg = (final[i] + final[i+1]) / 2
-            final[i] = avg + 0.35
-            final[i+1] = avg - 0.35
-        }
-    }
+// Parse data to js variable types.
+function parse(d) {
+    return {
+        "Date": d3.timeParse("%Y-%m-%d")(d.Date),
+        "Title": d.Show,
+        "Scores": d.Scores,
+    };
+};
 
-    var cy = yScale(final[0]);
-    for (const [i, score] of final.entries()) {
-        _legend.append("g")
-            .append("circle")
-                .attr("cx", 8)
-                .attr("cy", yScale(final[i]))
-                .attr("r", pointSize - 2)
-                .attr("fill", colors[i])
+var totaldb = null // TODO: fix var names / scope.
 
-        _legend.append("text")
-            .attr("x", 14)
-            .attr("y", yScale(final[i]) + 3.7)
-            .attr("font-size", 10)
-            .attr("font-family", "arial")
-            .text(corps[i])
+d3.dsv("|", "./data/scores.csv", parse).then(
+    (data) => {
 
-        cy += 20
-
-    }
-
-}
-
-
-
-Promise.all(corps.map(corp => d3.csv(`data/2024/${corp}.csv`))).then( 
-    data => {
-        data.map((corp) => {
-            corp.map((score) => {
-                score.date = d3.timeParse("%Y-%m-%d")(score.date)
-                score.score = parseFloat(score.score)
-            })
-        })
-
-        data = data.map((d, idx) => [corps[idx], d]);
+        // List of all corps.
+        const corpsList = WORLD_CLASS.concat(OPEN_CLASS);
         
-        plot(data)
-        legend(data)
+        // Map corps to their data.
+        const db = corpsList.map(corp => ({ "name": corp,  "scores": getCorp(data, corp, 2024, "Total") }));
+
+        totaldb = db; //TODO: GET RID OF ME
+
+        // Colors for each group.
+        const colorGen = d3.scaleOrdinal()
+            .domain(corpsList)
+            .range(d3.schemeSet2);
+            
+        const dates = db.flatMap(team => team.scores.map(score => new Date(score.date)));
+        xAxis = updateXAxis(d3.extent(dates))
+        const totals = db.flatMap(team => team.scores.map(score => new Date(score.score)));
+        yAxis = updateYAxis(d3.extent(totals));
+
+        const line = d3.line()
+            .x(d => xScale(d.date))
+            .y(d => yScale(+d.score))
+
+        plot.selectAll("myLines")
+            .data(db)
+            .join("path")
+                .attr("class", d => d.name.replaceAll(" ", "-"))
+                .attr("d", d => line(d.scores))
+                .attr("stroke", d => colorGen(d.name))
+                .style("stroke-width", 4)
+                .style("fill", "none")
+        
+        plot
+            .selectAll("dots")
+            .data(db)
+            .join('g')
+                .attr("class", d => d.name.replaceAll(" ", "-"))
+                .style("fill", d => colorGen(d.name))
+            .selectAll("points")
+            .data(d => d.scores)
+            .join("circle")
+                .attr("cx", d => xScale(d.date))
+                .attr("cy", d => yScale(+d.score))
+                .attr("r", 5)
+                .attr("stroke", "white")
+        
+        db.map(d => d3.selectAll("." + d.name.replaceAll(" ", "-")).transition().style("opacity", 0))
+
+        createLegend("World", WORLD_CLASS, legWidth * 1 / 3);
+        createLegend("Open", OPEN_CLASS, legWidth * 2 / 3);
+        
     }
-    
 );
